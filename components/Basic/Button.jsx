d920409d33cc87835c2e67989701725ad844eed5
IT20097042 <87925907+IT20097042@users.noreply.github.com>
2022-06-04 19:44:19 +0530

    Implementing the UI Components

---
+++ components/Basic/Button.jsx
@@ -0,0 +1,5 @@
+export default function Button(props){
+    return(
+        <button onClick={props.onclick} style={{backgroundColor: props.color}} className="btn btn-primary"> {props.text}</button>
+    )
+}
